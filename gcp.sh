8356e9c9a821f2498ffdb65ed333d957affffeba
一键安装脚本 <vsyour@qq.com>
2021-04-22 03:48:23 +0800

    Update gcp.sh

--- gcp.sh
+++ gcp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "----------1. 购买VPS主机：https://www.vultr.com/?ref=8391117-6G----------";sleep 5;
-echo "----------2. Blog: https://vksec.com----------";sleep 5;
+echo "----------1. 购买VPS主机：https://www.vultr.com/?ref=8391117-6G";sleep 5;
+echo "----------2. Blog: https://vksec.com";sleep 5;
 echo "----------3. generate ssh----------";sleep 5;
 id_rsa_path="${HOME}/.ssh/id_rsa"
 id_rsa_pub_path="${HOME}/.ssh/id_rsa.pub"
@@ -8,23 +8,23 @@ auth_path="${HOME}/.ssh/authorized_keys"
 ssh_path="${HOME}/.ssh/"
 if [ ! -d "$ssh_path" ]
 then
-        mkdir "$ssh_path"
-        ssh-keygen
-else
-        echo "ssh floder already exists"
-fi
+                mkdir "$ssh_path"
+                        ssh-keygen
+                else
+                                echo "ssh floder already exists"
+                        fi
 
-if [ ! -f "$auth_path" ]
-then
-        cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-else
-        echo "authorized_keys already exists"
-fi
-echo "----------restart ssh----------"
-sudo service ssh restart
-echo "----------shell id_rsa----------"
-cat ~/.ssh/id_rsa
-echo "----------shell IP----------"
-curl members.3322.org/dyndns/getip
-echo "----------shell user----------"
-echo "$USER"
+                        if [ ! -f "$auth_path" ]
+                        then
+                                        cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+                                else
+                                                echo "authorized_keys already exists"
+                                        fi
+                                        echo "----------restart ssh----------"
+                                        sudo service ssh restart
+                                        echo "----------shell id_rsa----------"
+                                        cat ~/.ssh/id_rsa
+                                        echo "----------shell IP----------"
+                                        curl members.3322.org/dyndns/getip
+                                        echo "----------shell user----------"
+                                        echo "$USER"
